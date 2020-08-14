7a1125f5a57b49ca4b825bab924df71e8d2c768f
jameskeith <james.keith@tealium.com>
2020-08-14 15:56:38 +0100

    webview session counting

--- tagmanagementdispatcher/src/test/java/com/tealium/tagmanagementdispatcher/TagManagementDispatcherTest.kt
+++ tagmanagementdispatcher/src/test/java/com/tealium/tagmanagementdispatcher/TagManagementDispatcherTest.kt
@@ -21,6 +21,7 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import java.io.File
+import java.net.URL
 
 class TagManagementDispatcherTest {
 
@@ -52,6 +53,7 @@ class TagManagementDispatcherTest {
 
         mockkConstructor(TealiumConfig::class)
         every { mockContext.filesDir } returns mockFile
+        every { mockContext.resources } returns mockk(relaxed = true)
         every { anyConstructed<TealiumConfig>().tealiumDirectory.mkdir() } returns mockk()
         every { anyConstructed<TealiumConfig>().tealiumDirectory.absolutePath } returns ""
 
@@ -84,10 +86,20 @@ class TagManagementDispatcherTest {
     @Test
     fun tagManagementDispatcherHasValidDefaultUrl() {
         val tagManagementDispatcher = TagManagementDispatcher(mockTealiumContext, mockDispatchSendCallbacks)
-        Assert.assertEquals(
-                "https://tags.tiqcdn.com/utag/${config.accountName}/${config.profileName}/${config.environment.environment}/mobile.html",
-                tagManagementDispatcher.urlString
-        )
+        val url = URL(tagManagementDispatcher.urlString)
+
+        Assert.assertEquals("https", url.protocol)
+        Assert.assertEquals("tags.tiqcdn.com", url.authority)
+        Assert.assertEquals("/utag/${config.accountName}/${config.profileName}/${config.environment.environment}/mobile.html", url.path)
+        Assert.assertEquals("/utag/${config.accountName}/${config.profileName}/${config.environment.environment}/mobile.html", url.path)
+
+        val queryParams = url.query.split("&").associate { param ->
+            Pair(param.split("=")[0], param.split("=")[1])
+        }
+        Assert.assertEquals("android", queryParams[DeviceCollectorConstants.DEVICE_PLATFORM])
+        Assert.assertEquals(/* Build.VERSION.RELEASE */"null", queryParams[DeviceCollectorConstants.DEVICE_OS_VERSION])
+        Assert.assertEquals(BuildConfig.VERSION_NAME, queryParams[CoreConstant.LIBRARY_VERSION])
+        Assert.assertEquals("true", queryParams["sdk_session_count"])
     }
 
     @Test
--- tagmanagementdispatcher/src/test/java/com/tealium/tagmanagementdispatcher/WebViewLoaderTest.kt
+++ tagmanagementdispatcher/src/test/java/com/tealium/tagmanagementdispatcher/WebViewLoaderTest.kt
@@ -9,16 +9,19 @@ import com.tealium.core.TealiumConfig
 import com.tealium.core.TealiumContext
 import com.tealium.core.messaging.AfterDispatchSendCallbacks
 import com.tealium.core.network.ConnectivityRetriever
+import com.tealium.core.network.HttpClient
 import io.mockk.*
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.setMain
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import java.io.File
+import java.util.concurrent.atomic.AtomicBoolean
 
 class WebViewLoaderTest {
 
@@ -40,6 +43,9 @@ class WebViewLoaderTest {
     @RelaxedMockK
     lateinit var mockTealiumContext: TealiumContext
 
+    @RelaxedMockK
+    lateinit var mockHttpClient: HttpClient
+
     lateinit var config: TealiumConfig
     lateinit var webViewLoader: WebViewLoader
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
@@ -58,6 +64,7 @@ class WebViewLoaderTest {
         config = TealiumConfig(mockContext, "test", "profile", Environment.QA)
         every { config.application.applicationContext } returns mockContext
         every { mockTealiumContext.config } returns config
+        every { mockTealiumContext.httpClient } returns mockHttpClient
 
         mockkConstructor(WebView::class)
         every {
@@ -96,5 +103,67 @@ class WebViewLoaderTest {
         webViewLoader.isWebViewLoaded.set(PageStatus.LOADED_ERROR)
 
         Assert.assertFalse(webViewLoader.isWebViewLoaded.get())
+    }
+
+    @Test
+    fun newSession_IsRegisteredWhenWebViewIsLoaded() {
+        webViewLoader = spyk(WebViewLoader(mockTealiumContext, "testUrl", mockDispatchSendCallbacks),
+                recordPrivateCalls = true)
+        every { webViewLoader.isWebViewLoaded.get() } returns true
+        webViewLoader.onSessionStarted(12345L)
+
+        coVerify {
+           mockHttpClient.get("https://tags.tiqcdn.com/utag/tiqapp/utag.v.js?a=test/profile/12345&cb=12345")
+        }
+    }
+
+    @Test
+    fun newSession_IsNotRegisteredWhenWebViewIsNotLoaded() = runBlocking {
+        webViewLoader = spyk(WebViewLoader(mockTealiumContext, "testUrl", mockDispatchSendCallbacks),
+                recordPrivateCalls = true)
+        val webViewLoaded = mockk<AtomicBoolean>()
+        every { webViewLoader.isWebViewLoaded } returns webViewLoaded
+        every { webViewLoaded.get() } returns false
+        webViewLoader.onSessionStarted(12345L)
+
+        coVerify {
+            mockHttpClient.get("https://tags.tiqcdn.com/utag/tiqapp/utag.v.js?a=test/profile/12345&cb=12345")
+        }
+    }
+
+    @Test
+    fun newSession_IsNotRegisteredWhenSessionIdIsInvalid() = runBlocking {
+        webViewLoader = spyk(WebViewLoader(mockTealiumContext, "testUrl", mockDispatchSendCallbacks),
+                recordPrivateCalls = true)
+        val webViewLoaded = mockk<AtomicBoolean>()
+        every { webViewLoader.isWebViewLoaded } returns webViewLoaded
+        every { webViewLoaded.get() } returns true
+        webViewLoader.onSessionStarted(WebViewLoader.INVALID_SESSION_ID)
+
+        coVerify(exactly = 0) {
+            mockHttpClient.get("https://tags.tiqcdn.com/utag/tiqapp/utag.v.js?a=test/profile/12345&cb=12345")
+        }
+    }
+
+    @Test
+    fun newSession_IsNotRegisteredWhenNoConnectivity() = runBlocking {
+        every { anyConstructed<ConnectivityRetriever>().isConnected() } returns false
+        webViewLoader = spyk(WebViewLoader(mockTealiumContext, "testUrl", mockDispatchSendCallbacks),
+                recordPrivateCalls = true)
+        val webViewLoaded = mockk<AtomicBoolean>()
+        every { webViewLoader.isWebViewLoaded } returns webViewLoaded
+        every { webViewLoaded.get() } returns true
+
+        webViewLoader.onSessionStarted(WebViewLoader.INVALID_SESSION_ID)
+
+        coVerify(exactly = 0) {
+            mockHttpClient.get("https://tags.tiqcdn.com/utag/tiqapp/utag.v.js?a=test/profile/12345&cb=12345")
+        }
+    }
+
+    @Test
+    fun createSessionUrlIsCorrect() {
+        Assert.assertEquals("https://tags.tiqcdn.com/utag/tiqapp/utag.v.js?a=test/profile/12345&cb=12345",
+                WebViewLoader.createSessionUrl(config, 12345L))
     }
 }
