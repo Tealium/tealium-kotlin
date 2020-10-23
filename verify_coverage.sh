3b7175214e99189dc3aba54e608acd23427e7cda
jameskeith <james.keith@tealium.com>
2020-10-23 14:37:00 +0100

    test fix +  zip

--- verify_coverage.sh
+++ verify_coverage.sh
@@ -4,13 +4,18 @@
 updatedModules
 
 echo "Checking test coverage: "
+reportsDir="reports"
+mkdir "$reportsDir"
 for mod in $updated_modules
 do
-  echo "./gradlew $mod:verifyTestCoverage"
-  if ./gradlew $mod":verifyTestCoverage"; then
+  echo "./gradlew $mod:jacocoTestReport $mod:verifyTestCoverage"
+  if ./gradlew $mod:jacocoTestReport $mod":verifyTestCoverage"; then
     echo "$mod Test Coverage Succeeded" >&2
   else
     echo "$mod Test Coverage failed" >&2
+
+    #TODO: get access to the coverage stats and report
     exit 1
   fi
+  zip -r "$reportsDir/${mod}.zip" "$mod/build/reports/"
 done
