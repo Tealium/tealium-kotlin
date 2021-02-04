1a4d3a763f053b855067710b1a2f4423b21f1a26
tamayok <>
2021-02-03 16:16:49 -0800

    MediaSessionDispatcher segment map update

--- media/src/main/java/com/tealium/media/MediaSessionDispatcher.kt
+++ media/src/main/java/com/tealium/media/MediaSessionDispatcher.kt
@@ -6,6 +6,7 @@ import com.tealium.media.segments.Segment
 
 class MediaSessionDispatcher(private val context: TealiumContext) : MediaDispatcher {
 
+    @Suppress("UNCHECKED_CAST")
     override fun track(event: String, mediaContent: MediaContent, segment: Segment?) {
         val data = mutableMapOf<String, Any>()
 
@@ -14,7 +15,9 @@ class MediaSessionDispatcher(private val context: TealiumContext) : MediaDispatc
         segment?.let {
             // merge MediaContent metadata with Chapter metadata (chapter data overwrites mediaContent data)
             if (segment.segmentInfo().containsKey(ChapterKey.METADATA)) {
-                mediaContent.metadata.putAll(segment.segmentInfo()[ChapterKey.METADATA] as Map<String, Any>)
+                (segment.segmentInfo()[ChapterKey.METADATA] as? Map<*,*>)?.let { map ->
+                    mediaContent.metadata.putAll(map as Map<String, Any>)
+                }
             }
 
             data.putAll(segment.segmentInfo().filter { (key, value) ->
