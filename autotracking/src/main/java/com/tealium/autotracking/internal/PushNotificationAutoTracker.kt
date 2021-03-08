e3cbf36f2c42dcfa1fb070e599c2e737b6e0ed85
jameskeith <james.keith@tealium.com>
2021-03-08 13:21:06 +0000

    pr fixes

--- autotracking/src/main/java/com/tealium/autotracking/internal/PushNotificationAutoTracker.kt
+++ autotracking/src/main/java/com/tealium/autotracking/internal/PushNotificationAutoTracker.kt
@@ -7,13 +7,6 @@ import com.tealium.dispatcher.TealiumEvent
 
 class PushNotificationAutoTracker(private val context: TealiumContext) : PushNotificationTracker {
 
-    private val PUSH_NOTIFICATION_PREFIX: String = "push_notification"
-    private val PUSH_NOTIFICATION_EVENT_NAME: String = "${PUSH_NOTIFICATION_PREFIX}_received"
-    private val PUSH_NOTIFICATION_TITLE: String = "${PUSH_NOTIFICATION_PREFIX}_title"
-    private val PUSH_NOTIFICATION_BODY: String = "${PUSH_NOTIFICATION_PREFIX}_body"
-    private val PUSH_NOTIFICATION_CATEGORY: String = "${PUSH_NOTIFICATION_PREFIX}_category"
-    private val PUSH_NOTIFICATION_TYPE: String = "${PUSH_NOTIFICATION_PREFIX}_type"
-
     override fun trackPushNotification(remoteMessage: RemoteMessage) {
         val eventData = mutableMapOf<String, Any>()
 
@@ -38,5 +31,14 @@ class PushNotificationAutoTracker(private val context: TealiumContext) : PushNot
         }
 
         context.track(TealiumEvent(PUSH_NOTIFICATION_EVENT_NAME, eventData))
+    }
+
+    private companion object {
+        private const val PUSH_NOTIFICATION_PREFIX: String = "push_notification"
+        private const val PUSH_NOTIFICATION_EVENT_NAME: String = "${PUSH_NOTIFICATION_PREFIX}_received"
+        private const val PUSH_NOTIFICATION_TITLE: String = "${PUSH_NOTIFICATION_PREFIX}_title"
+        private const val PUSH_NOTIFICATION_BODY: String = "${PUSH_NOTIFICATION_PREFIX}_body"
+        private const val PUSH_NOTIFICATION_CATEGORY: String = "${PUSH_NOTIFICATION_PREFIX}_category"
+        private const val PUSH_NOTIFICATION_TYPE: String = "${PUSH_NOTIFICATION_PREFIX}_type"
     }
 }
