a4ab41d7124fed111b82e8247b43d45056862fdd
James Keith <james.keith@tealium.com>
2021-04-20 16:15:22 +0100

    enable/disable remote_api

--- tagmanagementdispatcher/src/main/java/com/tealium/tagmanagementdispatcher/TealiumConfigTagManagementDispatcher.kt
+++ tagmanagementdispatcher/src/main/java/com/tealium/tagmanagementdispatcher/TealiumConfigTagManagementDispatcher.kt
@@ -3,6 +3,7 @@ package com.tealium.tagmanagementdispatcher
 import com.tealium.core.TealiumConfig
 
 const val TAG_MANAGEMENT_OVERRIDE_URL = "override_tag_management_url"
+const val TAG_MANAGEMENT_REMOTE_API_ENABLED = "tag_management_remote_api_enabled"
 
 /**
  * Sets the URL to use for the Tag Management module.
@@ -13,5 +14,18 @@ var TealiumConfig.overrideTagManagementUrl: String?
     set(value) {
         value?.let {
             options[TAG_MANAGEMENT_OVERRIDE_URL] = it
+        }
+    }
+
+/**
+ * Enables/Disables `remote_api` events from being sent to the Tealium WebView
+ *
+ * A value of `false` will stop TagManagement based RemoteCommands from functioning correctly.
+ */
+var TealiumConfig.remoteApiEnabled: Boolean?
+    get() = options[TAG_MANAGEMENT_REMOTE_API_ENABLED] as? Boolean
+    set(value) {
+        value?.let {
+            options[TAG_MANAGEMENT_REMOTE_API_ENABLED] = it
         }
     }
