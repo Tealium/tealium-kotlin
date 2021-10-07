fec520add3be98de7c60d543f05ed22892e9c69f
James Keith <james.keith@tealium.com>
2021-10-07 08:05:51 +0100

    router fix

--- tealiumlibrary/src/test/java/com/tealium/core/messaging/EventRouterTests.kt
+++ tealiumlibrary/src/test/java/com/tealium/core/messaging/EventRouterTests.kt
@@ -2,10 +2,12 @@ package com.tealium.core.messaging
 
 import com.tealium.dispatcher.Dispatch
 import io.mockk.*
+import kotlinx.coroutines.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import java.util.concurrent.Executors
 
 @RunWith(RobolectricTestRunner::class)
 class EventRouterTests {
@@ -87,6 +89,64 @@ class EventRouterTests {
 
         verify {
             listener.onRevalidate(any())
+        }
+    }
+
+    @Test
+    fun testAddingListenerDuringIteration_DoesNotThrow() = runBlocking {
+        val listeners: List<ActivityObserverListener> = (0..10).map {
+            mockk<ActivityObserverListener>(relaxed = true).also { eventRouter.subscribe(it) }
+        }
+        val listener1 = listeners[0]
+        val subUnsub = CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher()).async {
+            (0..100).forEach {
+                if (it % 2 == 0) {
+                    eventRouter.unsubscribe(listener1)
+                } else {
+                    eventRouter.subscribe(listener1)
+                }
+            }
+        }
+        val send = CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher()).async {
+            (0..100).forEach { _ ->
+                eventRouter.onActivityResumed()
+            }
+        }
+
+        val result = send.await()
+        val subUnsubResult = subUnsub.await()
+
+        verify (exactly = 101) {
+            listeners[1].onActivityResumed()
+        }
+    }
+
+    @Test
+    fun testAddingListenerDuringIteration_DoesNotThrow_Send() = runBlocking {
+        val listeners: List<ValidationChangedListener> = (0..10).map {
+            mockk<ValidationChangedListener>(relaxed = true).also { eventRouter.subscribe(it) }
+        }
+        val listener1 = listeners[0]
+        val subUnsub = CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher()).async {
+            (0..100).forEach {
+                if (it % 2 == 0) {
+                    eventRouter.unsubscribe(listener1)
+                } else {
+                    eventRouter.subscribe(listener1)
+                }
+            }
+        }
+        val send = CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher()).async {
+            (0..100).forEach { _ ->
+                eventRouter.send(ValidationChangedMessenger())
+            }
+        }
+
+        val result = send.await()
+        val subUnsubResult = subUnsub.await()
+
+        verify (exactly = 101) {
+            listeners[1].onRevalidate(any())
         }
     }
 }
