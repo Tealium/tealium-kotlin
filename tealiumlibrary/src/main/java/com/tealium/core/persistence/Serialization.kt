e578bec88ed26035e009b99286ef7c9fc61f34ef
James Keith <james.keith@tealium.com>
2021-11-10 19:24:51 +0000

    Mt 607 (#145)
    * wip datalayer
    
    * wip
    
    * tests
    
    * formatting
    
    * tidy
    
    * pr fixes

--- tealiumlibrary/src/main/java/com/tealium/core/persistence/Serialization.kt
+++ tealiumlibrary/src/main/java/com/tealium/core/persistence/Serialization.kt
@@ -1,9 +1,19 @@
 package com.tealium.core.persistence
 
-interface StringSerializer<T> {
-    fun serialize(): String
-}
+import org.json.JSONArray
+import org.json.JSONObject
 
-interface StringDeserializer<T> {
-    fun deserialize(string: String): T
+enum class Serialization(val code: Int, val clazz: Class<*>) {
+    STRING(0, String::class.java),
+    INT(1, Int::class.java),
+    DOUBLE(2, Double::class.java),
+    LONG(3, Long::class.java),
+    BOOLEAN(4, Boolean::class.java),
+    STRING_ARRAY(5, Array<String>::class.java),
+    INT_ARRAY(6, IntArray::class.java),
+    DOUBLE_ARRAY(7, DoubleArray::class.java),
+    LONG_ARRAY(8, LongArray::class.java),
+    BOOLEAN_ARRAY(9, BooleanArray::class.java),
+    JSON_OBJECT(10, JSONObject::class.java),
+    JSON_ARRAY(11, JSONArray::class.java);
 }
