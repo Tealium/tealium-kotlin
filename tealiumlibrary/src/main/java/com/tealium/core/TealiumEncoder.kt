708f513adcc0d50667044cb80cd8f96cb7e5dfd3
James Keith <james.keith@tealium.com>
2021-11-11 16:06:06 +0000

    encoder deprecation (#151)

--- tealiumlibrary/src/main/java/com/tealium/core/TealiumEncoder.kt
+++ tealiumlibrary/src/main/java/com/tealium/core/TealiumEncoder.kt
@@ -3,16 +3,19 @@ package com.tealium.core
 import com.tealium.dispatcher.Dispatch
 import java.net.URLEncoder
 
+@Deprecated("This feature will no longer be supported - prefer to use JsonUtils")
 interface Encoder {
     fun encode(dispatch: Dispatch): String
     fun encode(config: TealiumConfig): String
 }
 
+@Deprecated("This feature will no longer be supported - prefer to use JsonUtils")
 class TealiumEncoder private constructor() {
 
     companion object: Encoder {
         const val encoding = "UTF-8"
 
+        @Deprecated("This feature will no longer be supported - prefer to use JsonUtils")
         override fun encode(dispatch: Dispatch): String {
             val payload = dispatch.payload()
             val keys = dispatch.payload().keys
@@ -37,11 +40,13 @@ class TealiumEncoder private constructor() {
             return encoded.removeSuffix("&")
         }
 
+        @Deprecated("This feature will no longer be supported - prefer to use JsonUtils")
         override fun encode(config: TealiumConfig): String {
             return "${URLEncoder.encode(Dispatch.Keys.TEALIUM_ACCOUNT, encoding)}=${URLEncoder.encode(config.accountName, encoding)}&" +
                     "${URLEncoder.encode(Dispatch.Keys.TEALIUM_PROFILE, encoding)}=${URLEncoder.encode(config.profileName, encoding)}"
         }
 
+        @Deprecated("This feature will no longer be supported - prefer to use JsonUtils")
         private fun <T> encodeCollection(key: String, value: Array<T>): String {
             var encoded = ""
             value.forEach {
@@ -50,6 +55,7 @@ class TealiumEncoder private constructor() {
             return "${URLEncoder.encode(key, encoding)}=${URLEncoder.encode(encoded.removeSuffix(","), encoding)}"
         }
 
+        @Deprecated("This feature will no longer be supported - prefer to use JsonUtils")
         private fun <T> encode(key: String, value: T): String {
             return "${URLEncoder.encode(key, encoding)}=${URLEncoder.encode(value.toString(), encoding)}"
         }
