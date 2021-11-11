058d8db59030bdb0666fd0d55d343477b404adcf
James Keith <james.keith@tealium.com>
2021-11-11 15:05:43 +0000

    Release/vs 1.0.6 (#150)
    * fix parse errors
    
    * pr fix

--- visitorservice/src/main/java/com/tealium/visitorservice/CurrentVisit.kt
+++ visitorservice/src/main/java/com/tealium/visitorservice/CurrentVisit.kt
@@ -1,24 +1,23 @@
 package com.tealium.visitorservice
 
-import com.tealium.core.JsonUtils
-import org.json.JSONArray
 import org.json.JSONObject
 
 /**
  * Holds all visit-scoped attribute data relating to the current visitor identified by the [Tealium.visitorId].
  */
 data class CurrentVisit(
-        var createdAt: Long = System.currentTimeMillis(),
-        var totalEventCount: Int = 0,
-        var dates: Map<String, Long>? = null,
-        var booleans: Map<String, Boolean>? = null,
-        var arraysOfBooleans: Map<String, List<Boolean>>? = null,
-        var numbers: Map<String, Double>? = null,
-        var arraysOfNumbers: Map<String, List<Double>>? = null,
-        var tallies: Map<String, Map<String, Double>>? = null,
-        var strings: Map<String, String>? = null,
-        var arraysOfStrings: Map<String, List<String>>? = null,
-        var setsOfStrings: Map<String, Set<String>>? = null) {
+    var createdAt: Long = System.currentTimeMillis(),
+    var totalEventCount: Int = 0,
+    var dates: Map<String, Long>? = null,
+    var booleans: Map<String, Boolean>? = null,
+    var arraysOfBooleans: Map<String, List<Boolean>>? = null,
+    var numbers: Map<String, Double>? = null,
+    var arraysOfNumbers: Map<String, List<Double>>? = null,
+    var tallies: Map<String, Map<String, Double>>? = null,
+    var strings: Map<String, String>? = null,
+    var arraysOfStrings: Map<String, List<String>>? = null,
+    var setsOfStrings: Map<String, Set<String>>? = null
+) {
 
     companion object {
 
@@ -68,98 +67,15 @@ data class CurrentVisit(
             return CurrentVisit().apply {
                 createdAt = json.optLong(KEY_CREATED_AT)
                 totalEventCount = json.optInt(KEY_TOTAL_EVENT_COUNT)
-                dates = json.optJSONObject(KEY_DATES)?.let {
-                    JsonUtils.mapFor(it)
-                            .entries
-                            .associate { entry ->
-                                entry.key to entry.value as Long
-                            }
-                }
-                booleans = json.optJSONObject(KEY_FLAGS)?.let {
-                    JsonUtils.mapFor(it)
-                            .entries
-                            .associate { entry ->
-                                entry.key to entry.value as Boolean
-                            }
-                }
-                arraysOfBooleans = json.optJSONObject(KEY_FLAG_LISTS)?.let {
-                    JsonUtils.mapFor(it)
-                            .entries
-                            .associate { entry ->
-                                val values = entry.value as JSONArray
-                                val booleans = ArrayList<Boolean>()
-                                for (i in 0 until values.length()) {
-                                    booleans.add(values.get(i) as Boolean)
-                                }
-                                entry.key to booleans
-                            }
-                }
-                numbers = json.optJSONObject(KEY_METRICS)?.let {
-                    JsonUtils.mapFor(it)
-                            .entries
-                            .associate { entry ->
-                                entry.key to VisitorProfile.toDouble(entry.value)
-                            }.mapNotNull { (k, v) -> v?.let { k to it }  }.toMap()
-                }
-                arraysOfNumbers = json.optJSONObject(KEY_METRIC_LISTS)?.let {
-                    JsonUtils.mapFor(it)
-                            .entries
-                            .associate { entry ->
-                                val values = entry.value as JSONArray
-                                val doubles = ArrayList<Double>()
-                                for (i in 0 until values.length()) {
-                                    VisitorProfile.toDouble(values.get(i))?.let {
-                                        doubles.add(it)
-                                    }
-                                }
-                                entry.key to doubles
-                            }
-                }
-                tallies = json.optJSONObject(KEY_METRIC_SETS)?.let {
-                    JsonUtils.mapFor(it)
-                            .entries
-                            .associate { entry ->
-                                val values = entry.value as JSONObject
-                                val tallyValues = JsonUtils.mapFor(values)
-                                        .entries
-                                        .associate { tally ->
-                                            tally.key to VisitorProfile.toDouble(tally.value)
-                                        }.mapNotNull { (k, v) -> v?.let { k to it }  }.toMap()
-                                entry.key to tallyValues
-                            }
-                }
-                strings = json.optJSONObject(KEY_PROPERTIES)?.let {
-                    JsonUtils.mapFor(it)
-                            .entries
-                            .associate { entry ->
-                                entry.key to entry.value as String
-                            }
-                }
-                arraysOfStrings = json.optJSONObject(KEY_PROPERTY_LISTS)?.let {
-                    JsonUtils.mapFor(it)
-                            .entries
-                            .associate { entry ->
-                                val values = entry.value as JSONArray
-                                val strings = ArrayList<String>()
-                                for (i in 0 until values.length()) {
-                                    strings.add(values.get(i) as String)
-                                }
-                                entry.key to strings
-                            }
-
-                }
-                setsOfStrings = json.optJSONObject(KEY_PROPERTY_SETS)?.let {
-                    JsonUtils.mapFor(it)
-                            .entries
-                            .associate { entry ->
-                                val values = entry.value as JSONArray
-                                val strings = mutableSetOf<String>()
-                                for (i in 0 until values.length()) {
-                                    strings.add(values.get(i) as String)
-                                }
-                                entry.key to strings
-                            }
-                }
+                dates = json.optJSONObject(KEY_DATES)?.asDates()
+                booleans = json.optJSONObject(KEY_FLAGS)?.asBooleans()
+                arraysOfBooleans = json.optJSONObject(KEY_FLAG_LISTS)?.asArraysOfBooleans()
+                numbers = json.optJSONObject(KEY_METRICS)?.asNumbers()
+                arraysOfNumbers = json.optJSONObject(KEY_METRIC_LISTS)?.asArraysOfNumbers()
+                tallies = json.optJSONObject(KEY_METRIC_SETS)?.asTallies()
+                strings = json.optJSONObject(KEY_PROPERTIES)?.asStrings()
+                arraysOfStrings = json.optJSONObject(KEY_PROPERTY_LISTS)?.asArraysOfStrings()
+                setsOfStrings = json.optJSONObject(KEY_PROPERTY_SETS)?.asSetsOfStrings()
             }
         }
     }
