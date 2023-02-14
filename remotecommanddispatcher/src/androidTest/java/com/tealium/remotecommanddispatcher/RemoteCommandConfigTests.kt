6556969b315a94b652a1da6070ae9b704fe66b62
Karen Tamayo <karentamayo@Karens-MBP.webpass.net>
2023-02-14 14:10:43 -0800

    Remote Commands Static Mappings feature

--- remotecommanddispatcher/src/androidTest/java/com/tealium/remotecommanddispatcher/RemoteCommandConfigTests.kt
+++ remotecommanddispatcher/src/androidTest/java/com/tealium/remotecommanddispatcher/RemoteCommandConfigTests.kt
@@ -9,6 +9,8 @@ class RemoteCommandConfigTests {
     private val expectedConfig = mapOf<String, Any>("config_setting1" to "setting1", "config_setting2" to "setting2", "config_setting3" to "setting3", "config_setting4" to "setting4")
     private val expectedMappings = mapOf("mappings_test1" to "param_test1", "mappings_test2" to "param_test2", "mappings_test3" to "param_test3", "mappings_test4" to "param_test4", "mappings_test5" to "param_test5")
     private val expectedCommands = mapOf("launch" to "initialize", "screen_view" to "setscreenname", "click_thru" to "logevent", "social_media_activity" to "setuserid", "user_login" to "logevent")
+    private val expectedCompoundCommands = mapOf("tealium_event:launch" to "initialize", "screen_view:home,page_content:homeView" to "setscreenname", "click_thru" to "logevent", "social_media_activity" to "setuserid", "user_login" to "logevent")
+    private val expectedStatics = mapOf("launch" to mapOf("static_key1" to "launch_event", "static_key2" to "some_value"), "tealium_event:click,page_name:home" to mapOf("static_key" to "click_value"))
 
     @Test
     fun remoteCommandConfigValidParseFromJson() {
@@ -43,6 +45,38 @@ class RemoteCommandConfigTests {
     }
 
     @Test
+    fun remoteCommandConfigWithCompoundCommandsValidParseFromJson() {
+        val remoteCommandConfig = "{\n" +
+                "  \"config\": {\n" +
+                "  \"config_setting1\": \"setting1\",\n" +
+                "  \"config_setting2\": \"setting2\",\n" +
+                "  \"config_setting3\": \"setting3\",\n" +
+                "  \"config_setting4\": \"setting4\"\n" +
+                "  },\n" +
+                "  \"mappings\": {\n" +
+                "  \"mappings_test1\": \"param_test1\",\n" +
+                "  \"mappings_test2\": \"param_test2\",\n" +
+                "  \"mappings_test3\": \"param_test3\",\n" +
+                "  \"mappings_test4\": \"param_test4\",\n" +
+                "  \"mappings_test5\": \"param_test5\"\n" +
+                "  },\n" +
+                "  \"commands\": {\n" +
+                "  \"tealium_event:launch\": \"initialize\",\n" +
+                "  \"screen_view:home,page_content:homeView\": \"setscreenname\",\n" +
+                "  \"click_thru\": \"logevent\",\n" +
+                "  \"social_media_activity\": \"setuserid\",\n" +
+                "  \"user_login\": \"logevent\"\n" +
+                "  }\n" +
+                "}"
+        val json = JSONObject(remoteCommandConfig)
+        val config = RemoteCommandConfig.fromJson(json)
+
+        assertEquals(expectedConfig, config.apiConfig)
+        assertEquals(expectedMappings, config.mappings)
+        assertEquals(expectedCompoundCommands, config.apiCommands)
+    }
+
+    @Test
     fun remoteCommandConfigValidToJson() {
         val remoteCommandConfig = "{\n" +
                 "  \"config\": {\n" +
@@ -74,8 +108,160 @@ class RemoteCommandConfigTests {
     }
 
     @Test
+    fun remoteCommandConfigWithCompoundCommandsValidToJson() {
+        val remoteCommandConfig = "{\n" +
+                "  \"config\": {\n" +
+                "  \"config_setting1\": \"setting1\",\n" +
+                "  \"config_setting2\": \"setting2\",\n" +
+                "  \"config_setting3\": \"setting3\",\n" +
+                "  \"config_setting4\": \"setting4\"\n" +
+                "  },\n" +
+                "  \"mappings\": {\n" +
+                "  \"mappings_test1\": \"param_test1\",\n" +
+                "  \"mappings_test2\": \"param_test2\",\n" +
+                "  \"mappings_test3\": \"param_test3\",\n" +
+                "  \"mappings_test4\": \"param_test4\",\n" +
+                "  \"mappings_test5\": \"param_test5\"\n" +
+                "  },\n" +
+                "  \"commands\": {\n" +
+                "  \"tealium_event:launch\": \"initialize\",\n" +
+                "  \"screen_view:home,page_content:homeView\": \"setscreenname\",\n" +
+                "  \"click_thru\": \"logevent\",\n" +
+                "  \"social_media_activity\": \"setuserid\",\n" +
+                "  \"user_login\": \"logevent\"\n" +
+                "  }\n" +
+                "}"
+        val json = JSONObject(remoteCommandConfig)
+        val config = RemoteCommandConfig(apiConfig = expectedConfig, mappings = expectedMappings, apiCommands = expectedCompoundCommands)
+        val configJson = RemoteCommandConfig.toJson(config)
+
+        assertEquals(json.toString(), configJson.toString())
+    }
+
+    @Test
+    fun remoteCommandConfigWithDefaultDelimitersValidToJson() {
+        val remoteCommandConfig = "{\n" +
+                "  \"config\": {\n" +
+                "  \"config_setting1\": \"setting1\",\n" +
+                "  \"config_setting2\": \"setting2\",\n" +
+                "  \"config_setting3\": \"setting3\",\n" +
+                "  \"config_setting4\": \"setting4\"\n" +
+                "  },\n" +
+                "  \"mappings\": {\n" +
+                "  \"mappings_test1\": \"param_test1\",\n" +
+                "  \"mappings_test2\": \"param_test2\",\n" +
+                "  \"mappings_test3\": \"param_test3\",\n" +
+                "  \"mappings_test4\": \"param_test4\",\n" +
+                "  \"mappings_test5\": \"param_test5\"\n" +
+                "  },\n" +
+                "  \"commands\": {\n" +
+                "  \"launch\": \"initialize\",\n" +
+                "  \"screen_view\": \"setscreenname\",\n" +
+                "  \"click_thru\": \"logevent\",\n" +
+                "  \"social_media_activity\": \"setuserid\",\n" +
+                "  \"user_login\": \"logevent\"\n" +
+                "  }\n" +
+                "}"
+        val json = JSONObject(remoteCommandConfig)
+        val config = RemoteCommandConfig.fromJson(json)
+
+        assertEquals(Settings.DEFAULT_EQUALITY_DELIMITER, config.delimiters.keysEqualityDelimiter)
+        assertEquals(Settings.DEFAULT_SEPARATION_DELIMITER, config.delimiters.keysSeparationDelimiter)
+    }
+
+    @Test
+    fun remoteCommandConfigWithCustomDelimitersValidToJson() {
+        val remoteCommandConfig = "{\n" +
+                "  \"config\": {\n" +
+                "  \"config_setting1\": \"setting1\",\n" +
+                "  \"config_setting2\": \"setting2\",\n" +
+                "  \"keys_separation_delimiter\": \"&&\",\n" +
+                "  \"keys_equality_delimiter\": \"==\"\n" +
+                "  },\n" +
+                "  \"mappings\": {\n" +
+                "  \"mappings_test1\": \"param_test1\",\n" +
+                "  \"mappings_test2\": \"param_test2\",\n" +
+                "  \"mappings_test3\": \"param_test3\",\n" +
+                "  \"mappings_test4\": \"param_test4\",\n" +
+                "  \"mappings_test5\": \"param_test5\"\n" +
+                "  },\n" +
+                "  \"commands\": {\n" +
+                "  \"launch\": \"initialize\",\n" +
+                "  \"screen_view\": \"setscreenname\",\n" +
+                "  \"click_thru\": \"logevent\",\n" +
+                "  \"social_media_activity\": \"setuserid\",\n" +
+                "  \"user_login\": \"logevent\"\n" +
+                "  }\n" +
+                "}"
+        val json = JSONObject(remoteCommandConfig)
+        val config = RemoteCommandConfig.fromJson(json)
+
+        assertEquals("==", config.delimiters.keysEqualityDelimiter)
+        assertEquals("&&", config.delimiters.keysSeparationDelimiter)
+    }
+
+    @Test
+    fun remoteCommandConfigWithStaticsValidToJson() {
+        val remoteCommandConfig = "{\n" +
+                "  \"config\": {\n" +
+                "  \"config_setting1\": \"setting1\",\n" +
+                "  \"config_setting2\": \"setting2\",\n" +
+                "  \"config_setting3\": \"setting3\",\n" +
+                "  \"config_setting4\": \"setting4\"\n" +
+                "  },\n" +
+                "  \"mappings\": {\n" +
+                "  \"mappings_test1\": \"param_test1\",\n" +
+                "  \"mappings_test2\": \"param_test2\",\n" +
+                "  \"mappings_test3\": \"param_test3\",\n" +
+                "  \"mappings_test4\": \"param_test4\",\n" +
+                "  \"mappings_test5\": \"param_test5\"\n" +
+                "  },\n" +
+                "  \"commands\": {\n" +
+                "  \"launch\": \"initialize\",\n" +
+                "  \"screen_view\": \"setscreenname\",\n" +
+                "  \"click_thru\": \"logevent\",\n" +
+                "  \"social_media_activity\": \"setuserid\",\n" +
+                "  \"user_login\": \"logevent\"\n" +
+                "  },\n" +
+                "  \"statics\": {\n" +
+                "  \"launch\": {\n" +
+                "  \"static_key1\": launch_event,\n" +
+                "  \"static_key2\": \"some_value\"\n" +
+                "  },\n" +
+                "  \"tealium_event:click,page_name:home\": {\n" +
+                "  \"static_key\": \"click_value\"\n" +
+                "  }\n" +
+                "  }\n" +
+                "}"
+        val json = JSONObject(remoteCommandConfig)
+        val config = RemoteCommandConfig(apiConfig = expectedConfig, mappings = expectedMappings, apiCommands = expectedCommands, statics = expectedStatics)
+        val configJson = RemoteCommandConfig.toJson(config)
+
+        assertEquals(json.toString(), configJson.toString())
+
+    }
+
+    @Test
     fun remoteCommandConfigValidToFromJson() {
         val config = RemoteCommandConfig(apiConfig = expectedConfig, mappings = expectedMappings, apiCommands = expectedCommands)
+        val configJson = RemoteCommandConfig.toJson(config)
+        val configFromJson = RemoteCommandConfig.fromJson(configJson)
+
+        assertEquals(config, configFromJson)
+    }
+
+    @Test
+    fun remoteCommandConfigWithStaticsValidToFromJson() {
+        val config = RemoteCommandConfig(apiConfig = expectedConfig, mappings = expectedMappings, apiCommands = expectedCommands, statics = expectedStatics)
+        val configJson = RemoteCommandConfig.toJson(config)
+        val configFromJson = RemoteCommandConfig.fromJson(configJson)
+
+        assertEquals(config, configFromJson)
+    }
+
+    @Test
+    fun remoteCommandConfigWithStaticsAndCustomDelimitersValidToFromJson() {
+        val config = RemoteCommandConfig(apiConfig = expectedConfig, mappings = expectedMappings, apiCommands = expectedCommands, statics = expectedStatics, delimiters = Delimiters("==", "&&"))
         val configJson = RemoteCommandConfig.toJson(config)
         val configFromJson = RemoteCommandConfig.fromJson(configJson)
 
--- remotecommanddispatcher/src/main/java/com/tealium/remotecommanddispatcher/RemoteCommandConfig.kt
+++ remotecommanddispatcher/src/main/java/com/tealium/remotecommanddispatcher/RemoteCommandConfig.kt
@@ -3,31 +3,57 @@ package com.tealium.remotecommanddispatcher
 import com.tealium.core.JsonUtils
 import org.json.JSONObject
 
-data class RemoteCommandConfig(var apiConfig: Map<String, Any>? = null,
-                               var mappings: Map<String, String>? = null,
-                               var apiCommands: Map<String, String>? = null) {
+data class Delimiters(
+    var keysEqualityDelimiter: String = Settings.DEFAULT_EQUALITY_DELIMITER,
+    var keysSeparationDelimiter: String = Settings.DEFAULT_SEPARATION_DELIMITER
+)
+
+data class RemoteCommandConfig(
+    var apiConfig: Map<String, Any>? = null,
+    var mappings: Map<String, String>? = null,
+    var apiCommands: Map<String, String>? = null,
+    var statics: Map<String, Any>? = null,
+    var delimiters: Delimiters = Delimiters()
+) {
     companion object {
         fun fromJson(jsonObject: JSONObject): RemoteCommandConfig {
             val remoteCommandConfig = RemoteCommandConfig()
 
             jsonObject.optJSONObject(Settings.CONFIG)?.let {
+                if (it.has(Settings.KEYS_EQUALITY_DELIMITER)) {
+                    remoteCommandConfig.delimiters.keysEqualityDelimiter = it.getString(Settings.KEYS_EQUALITY_DELIMITER)
+                    it.remove(Settings.KEYS_EQUALITY_DELIMITER)
+                }
+                if (it.has(Settings.KEYS_SEPARATION_DELIMITER)) {
+                    remoteCommandConfig.delimiters.keysSeparationDelimiter = it.getString(Settings.KEYS_SEPARATION_DELIMITER)
+                    it.remove(Settings.KEYS_SEPARATION_DELIMITER)
+                }
+
                 remoteCommandConfig.apiConfig = JsonUtils.mapFor(it)
             }
 
             jsonObject.optJSONObject(Settings.MAPPINGS)?.let {
                 remoteCommandConfig.mappings = JsonUtils.mapFor(it)
-                        .entries
-                        .associate { entry ->
-                            entry.key to entry.value as String
-                        }
+                    .entries
+                    .associate { entry ->
+                        entry.key to entry.value as String
+                    }
             }
 
             jsonObject.optJSONObject(Settings.COMMANDS)?.let {
                 remoteCommandConfig.apiCommands = JsonUtils.mapFor(it)
-                        .entries
-                        .associate { entry ->
-                            entry.key to entry.value as String
-                        }
+                    .entries
+                    .associate { entry ->
+                        entry.key to entry.value as String
+                    }
+            }
+
+            jsonObject.optJSONObject(Settings.STATICS)?.let {
+                remoteCommandConfig.statics = JsonUtils.mapFor(it)
+                    .entries
+                    .associate { entry ->
+                        entry.key to JsonUtils.mapFor(entry.value as JSONObject)
+                    }
             }
 
             return remoteCommandConfig
@@ -35,14 +61,32 @@ data class RemoteCommandConfig(var apiConfig: Map<String, Any>? = null,
 
         fun toJson(remoteCommandConfig: RemoteCommandConfig): JSONObject {
             val json = JSONObject()
+
             remoteCommandConfig.apiConfig?.let {
-                json.put(Settings.CONFIG, JsonUtils.jsonFor(it))
+                val map = it.toMutableMap()
+
+                if (remoteCommandConfig.delimiters.keysEqualityDelimiter != Settings.DEFAULT_EQUALITY_DELIMITER) {
+                    map[Settings.KEYS_EQUALITY_DELIMITER] = remoteCommandConfig.delimiters.keysEqualityDelimiter
+                }
+
+                if (remoteCommandConfig.delimiters.keysSeparationDelimiter != Settings.DEFAULT_SEPARATION_DELIMITER) {
+                    map[Settings.KEYS_SEPARATION_DELIMITER] = remoteCommandConfig.delimiters.keysSeparationDelimiter
+                }
+
+                json.put(Settings.CONFIG, JsonUtils.jsonFor(map.toMap()))
             }
             remoteCommandConfig.mappings?.let {
                 json.put(Settings.MAPPINGS, JsonUtils.jsonFor(it))
             }
             remoteCommandConfig.apiCommands?.let {
                 json.put(Settings.COMMANDS, JsonUtils.jsonFor(it))
+            }
+            remoteCommandConfig.statics?.let {
+                val obj = JSONObject()
+                it.forEach { (key, value) ->
+                    obj.put(key, JsonUtils.jsonFor(value as Map<String, Any>))
+                }
+                json.put(Settings.STATICS, obj)
             }
 
             return json
