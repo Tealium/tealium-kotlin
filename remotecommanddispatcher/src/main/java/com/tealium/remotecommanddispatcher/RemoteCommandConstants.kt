8ed3afb644b5397df382efd84dd1d2c7f72213c5
Karen Tamayo <karen.tamayo@tealium.com>
2023-04-11 14:47:25 -0700

    [MT-1190] Remote Commands Static Mappings feature (#202)
    * Remote Commands Static Mappings feature
    ---------
    
    Co-authored-by: Karen Tamayo <karentamayo@Karens-MBP.webpass.net>

--- remotecommanddispatcher/src/main/java/com/tealium/remotecommanddispatcher/RemoteCommandConstants.kt
+++ remotecommanddispatcher/src/main/java/com/tealium/remotecommanddispatcher/RemoteCommandConstants.kt
@@ -6,10 +6,17 @@ object Settings {
     const val CONFIG = "config"
     const val MAPPINGS = "mappings"
     const val COMMANDS = "commands"
+    const val STATICS = "statics"
     const val COMMAND_NAME = "command_name"
 
     const val ALL_EVENTS = "all_events"
     const val ALL_VIEWS = "all_views"
+
+    const val KEYS_SEPARATION_DELIMITER = "keys_separation_delimiter"
+    const val KEYS_EQUALITY_DELIMITER = "keys_equality_delimiter"
+
+    const val DEFAULT_SEPARATION_DELIMITER = ","
+    const val DEFAULT_EQUALITY_DELIMITER = ":"
 }
 
 object Key {
