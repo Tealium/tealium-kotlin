b947f18db21d262341dcd05a0e6faa731f50ffe4
Karen Tamayo <karentamayo@Karens-MBP.webpass.net>
2023-05-22 15:57:10 -0700

    Remove headers map from ResourceEntity

--- tealiumlibrary/src/main/java/com/tealium/core/network/ResourceEntity.kt
+++ tealiumlibrary/src/main/java/com/tealium/core/network/ResourceEntity.kt
@@ -2,6 +2,5 @@ package com.tealium.core.network
 
 data class ResourceEntity(
     val response: String? = null,
-    val etag: String? = null,
-    val headers: Map<String, List<String>>? = null // keep all headers?
+    val etag: String? = null
 )
