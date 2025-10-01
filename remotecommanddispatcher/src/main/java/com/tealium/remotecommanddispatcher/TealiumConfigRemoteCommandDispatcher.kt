dd5f32b022610eb11639a2203c356ab160bdaa1e
Sebastian <sebastian.krajna@tealium.com>
2025-10-01 14:28:15 +0200

    Add remoteAPIEnabled configuration to TealiumConfig for controlling remote_api events

--- remotecommanddispatcher/src/main/java/com/tealium/remotecommanddispatcher/TealiumConfigRemoteCommandDispatcher.kt
+++ remotecommanddispatcher/src/main/java/com/tealium/remotecommanddispatcher/TealiumConfigRemoteCommandDispatcher.kt
@@ -3,6 +3,7 @@ package com.tealium.remotecommanddispatcher
 import com.tealium.core.TealiumConfig
 
 const val REMOTE_COMMAND_CONFIG_REFRESH = "remote_command_config_refresh"
+const val REMOTE_COMMAND_REMOTE_API_ENABLED = "remote_command_remote_api_enabled"
 
 /**
  * Optional config flag for the remote command dispatcher - determines the minimum number of minutes
@@ -14,5 +15,21 @@ var TealiumConfig.remoteCommandConfigRefresh: Long?
     set(value) {
         value?.let {
             options[REMOTE_COMMAND_CONFIG_REFRESH] = it
+        }
+    }
+
+/**
+ * Enables/Disables `remote_api` events from being sent.
+ *
+ * A value of `true` will enable remote_api events, required for RemoteCommands to function correctly.
+ * This is typically set automatically when the RemoteCommandDispatcher is added to the configuration.
+ *
+ * Default: false
+ */
+var TealiumConfig.remoteAPIEnabled: Boolean?
+    get() = options[REMOTE_COMMAND_REMOTE_API_ENABLED] as? Boolean
+    set(value) {
+        value?.let {
+            options[REMOTE_COMMAND_REMOTE_API_ENABLED] = it
         }
     }
